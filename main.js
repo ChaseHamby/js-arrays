faf4422daf2c4c3d0dcf5a34a4dbcff2f51c5c17
Chase Hamby <c.chase.hamby@gmail.com>
2018-08-28 19:25:16 -0500

    added for loop

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ console.log('newNum', newNum);
 
 // mini challenge
 
-
 const palChecker = (word) => {
     const reverseWord = word.split('').reverse().join('');
     if (word === reverseWord) {
@@ -24,3 +23,21 @@ const palChecker = (word) => {
 palChecker('mom');
 palChecker('racecar');
 palChecker('drink');
+
+let animals = ['lion', 'coyote', 'wolf', 'elephant'];
+
+const lastItem = animals.pop(); // elephant // removes last item
+const firstItem = animals.shift(); // lion // removes first item
+animals.push('bat'); // adds to the end
+animals.unshift('tiger'); // adds to the beginning
+console.log('animals:', animals);
+
+// for loops
+// 1. what is your starting value? 99% of the time it is 0
+// 2. what is your stopping value? usually is something.length
+// 3. how are you incrementing? +1 
+
+for(let i = 0; i < animals.length; i++){
+    // do something here
+    console.log(animals[i]);
+}
